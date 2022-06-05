4626c5c7a9505143580155ef9f2ed3728cab0f71
Ivo <ivansopena@gmail.com>
2022-06-05 05:57:06 +0200

    Mod: Delete comment

--- TrueketeaApp/TrueketeaApp/ViewModels/AddProductViewModel.cs
+++ TrueketeaApp/TrueketeaApp/ViewModels/AddProductViewModel.cs
@@ -228,7 +228,7 @@ namespace TrueketeaApp.ViewModels
                     }
                     else
                     {
-                        prd.NombreProducto = ProductName;//myView.FindByName<Entry>("Title").Text;
+                        prd.NombreProducto = ProductName;
                     }
 
                     if(price == "0")
